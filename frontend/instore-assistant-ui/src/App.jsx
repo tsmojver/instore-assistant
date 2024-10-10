ddddbe9600a8554fb3e9e54799eab05d8f5af73d
Aldo Barrera <aldo.barrera@dominos.com>
2024-10-10 14:17:10 -0600

    Add azure Speech SDK for mic input

--- frontend/instore-assistant-ui/src/App.jsx
+++ frontend/instore-assistant-ui/src/App.jsx
@@ -1,17 +1,15 @@
-import { useEffect, useState } from 'react'
-import { Chat } from "react-chat-module";
-import "react-chat-module/dist/index.css";
-import styles from './App.module.css';
-import ChatMessage from './components/ChatMessage';
-import c from 'classnames';
+import { useState } from "react";
+import styles from "./App.module.css";
+import ChatMessage from "./components/ChatMessage";
+import c from "classnames";
+import * as sdk from "microsoft-cognitiveservices-speech-sdk";
 
 const AZURE_API_KEY = "3d6f86c1346c47d4880bb548c6b304ce";
-const AZURE_BACKEND_URL = "https://odlu-m23ljsw2-eastus2.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2024-02-15-preview"
+const AZURE_BACKEND_URL =
+  "https://odlu-m23ljsw2-eastus2.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2024-02-15-preview";
 
-const testComponent = () => {
-  return <div>Test</div>;
-}
-
+var subscriptionKey = "bd12a82306f448b1bb537464ea58ca8c";
+var serviceRegion = "eastus";
 
 function App() {
   // add initial set of example messages
@@ -38,36 +36,39 @@ function App() {
   const [messages, setMessages] = useState([]);
 
   // const [allSpokenWords] = '';
-  const [promptValue, setPromptValue] = useState('')
+  const [promptValue, setPromptValue] = useState("");
+  var speechConfig = sdk.SpeechConfig.fromSubscription(
+    subscriptionKey,
+    serviceRegion
+  );
+  var audioConfig;
 
-  useEffect(() => {
-    console.log('Recording')
-    const recognition = new webkitSpeechRecognition();
-    recognition.lang = "en-US";
-    recognition.interimResults = false;
-    recognition.maxAlternatives = 1;
-    recognition.continuous = true;
+  const startMicrophoneInput = async () => {
+    audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
 
-    console.log('starting recognition')
-    recognition.start();
-    recognition.onresult = (event) => {
-      console.log(event)
-      const speechResult = event.results[event.results.length - 1][0].transcript;
-      console.log('Result:', speechResult)
-      setPromptValue(speechResult)
-    }
-    recognition.onend = () => {
-      console.log('ended')
-    }
-    recognition.onerror = (event) => {
-      console.log('Error:', event.error)
-    }
+    // setting the recognition language to English.
+    speechConfig.speechRecognitionLanguage = "en-US";
 
-    return () => {
-      console.log('stopping recognition')
-      recognition.stop();
-    }
-  }, [])
+    // create the speech recognizer.
+    var recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
+
+    // start the recognizer and wait for a result.
+    recognizer.recognizeOnceAsync(
+      function (result) {
+        console.log("Result:", result);
+        setPromptValue(result.text);
+
+        recognizer.close();
+        recognizer = undefined;
+      },
+      function (err) {
+        console.trace("err - " + err);
+
+        recognizer.close();
+        recognizer = undefined;
+      }
+    );
+  };
 
   // const onSend = async (message) => {
   //   // build new message received from chat component
@@ -110,58 +111,78 @@ function App() {
   // };
 
   const sendMessageToModel = async () => {
-    setMessages(messages => [...messages, { text: promptValue }])
+    setMessages((messages) => [...messages, { text: promptValue }]);
 
     try {
       const response = await fetch(AZURE_BACKEND_URL, {
-        method: 'POST',
+        method: "POST",
         headers: {
-          'Content-Type': 'application/json',
-          'api-key': AZURE_API_KEY
+          "Content-Type": "application/json",
+          "api-key": AZURE_API_KEY,
         },
         body: JSON.stringify({
           messages: [
             {
-              role: 'system',
-              content: [{
-                type: 'text',
-                text: promptValue
-              }],
-            }
-          ]
-        })
+              role: "system",
+              content: [
+                {
+                  type: "text",
+                  text: promptValue,
+                },
+              ],
+            },
+          ],
+        }),
       });
 
-      console.log('Success:', response);
+      console.log("Success:", response);
       const responseJSON = await response.json();
-      setMessages(messages => [...messages, { text: responseJSON?.choices[0]?.message?.content }])
+      setMessages((messages) => [
+        ...messages,
+        { text: responseJSON?.choices[0]?.message?.content },
+      ]);
       console.log(responseJSON?.choices[0]?.message?.content);
-      setPromptValue('')
+      setPromptValue("");
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
-  }
+  };
 
   // adding chat component in full screen container
   return (
     <div className={styles.App}>
-      {/* <Chat messages={messages} userId={"1"} onSend={onSend} disableAttachments customFactories={{
-        test: {
-          hasText: false,
-          factory: testComponent
-        }
-      }} /> */}
       <section className={styles.topNav}>In Store Assistant</section>
-      <section className={c(styles.messagesContainer, !messages.length && styles.noMessages)}>
-        {messages.length ? messages.map((message, index) => { return <ChatMessage key={index} isLeftAligned={index % 2 ? false : true}>{message.text}</ChatMessage> }) : <div>No messages</div>}
+      <section
+        className={c(
+          styles.messagesContainer,
+          !messages.length && styles.noMessages
+        )}
+      >
+        {messages.length ? (
+          messages.map((message, index) => {
+            return (
+              <ChatMessage key={index} isLeftAligned={index % 2 ? false : true}>
+                {message.text}
+              </ChatMessage>
+            );
+          })
+        ) : (
+          <div>No messages</div>
+        )}
       </section>
       <section className={styles.inputRow}>
-        <textarea className={styles.input} onInput={e => setPromptValue(e.currentTarget.value)} type="text" inputMode='text' value={promptValue} />
-        <button onClick={() => { }}>Record</button>
+        <textarea
+          className={styles.input}
+          onInput={(e) => setPromptValue(e.currentTarget.value)}
+          type="text"
+          inputMode="text"
+          value={promptValue}
+        />
+        <button onClick={startMicrophoneInput}>Record</button>
         <button onClick={sendMessageToModel}>Send</button>
       </section>
     </div>
   );
 }
 
-export default App
+export default App;
