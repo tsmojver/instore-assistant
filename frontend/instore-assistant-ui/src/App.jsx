d6e4cd3c59a3571acd4cff88b74002caf72c622d
Justin Brees <justin.brees@dominos.com>
2024-10-11 06:02:29 -0700

    Merged changes between main and justin

--- frontend/instore-assistant-ui/src/App.jsx
+++ frontend/instore-assistant-ui/src/App.jsx
@@ -1,58 +1,51 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import styles from "./App.module.css";
 import ChatMessage from "./components/ChatMessage";
 import c from "classnames";
-import * as sdk from "microsoft-cognitiveservices-speech-sdk";
+import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
 const AZURE_API_KEY = "3d6f86c1346c47d4880bb548c6b304ce";
 const AZURE_BACKEND_URL =
   "https://odlu-m23ljsw2-eastus2.openai.azure.com/openai/deployments/gpt-4o/chat/completions?api-version=2024-02-15-preview";
 
-var subscriptionKey = "bd12a82306f448b1bb537464ea58ca8c";
-var serviceRegion = "eastus";
-
 function App() {
-
   const [messages, setMessages] = useState([]);
-
-  // const [allSpokenWords] = '';
+  const [isListening, setIsListening]  = useState(false);
+  const audioRef = useRef();
   const [promptValue, setPromptValue] = useState("");
-  var speechConfig = sdk.SpeechConfig.fromSubscription(
-    subscriptionKey,
-    serviceRegion
-  );
-  var audioConfig;
+  
+  const handleHeyDom = () => {
+    setIsListening(true);
+    console.log('isListening');
+    audioRef.current.play();
+  }
+  const handleWords = (words) => {
+    console.log('handleWords', words, isListening);
+    if (isListening) {
+      setPromptValue(words);
+      sendMessageToModel(words);
+      setIsListening(false);
+    }
+  }
+  const commands = [
+    {
+      command: 'Hey Dom',
+      callback: () => handleHeyDom()
+    },
+    {
+      command: '*',
+      callback: (words) => handleWords(words)
+    }
+  ]
+  const { listening, transcript, isMicrophoneAvailable } = useSpeechRecognition({ commands });
 
-  const startMicrophoneInput = async () => {
-    audioConfig = sdk.AudioConfig.fromDefaultMicrophoneInput();
-
-    // setting the recognition language to English.
-    speechConfig.speechRecognitionLanguage = "en-US";
-
-    // create the speech recognizer.
-    var recognizer = new sdk.SpeechRecognizer(speechConfig, audioConfig);
-
-    // start the recognizer and wait for a result.
-    recognizer.recognizeOnceAsync(
-      function (result) {
-        console.log("Result:", result);
-        setPromptValue(result.text);
-
-        recognizer.close();
-        recognizer = undefined;
-      },
-      function (err) {
-        console.trace("err - " + err);
-
-        recognizer.close();
-        recognizer = undefined;
-      }
-    );
-  };
-
-  const sendMessageToModel = async () => {
-    setMessages((messages) => [...messages, { text: promptValue }]);
-
+  
+  useEffect(() => {
+    console.log('isListening', listening, isListening, transcript, isMicrophoneAvailable);
+  }, [listening, isListening, transcript, isMicrophoneAvailable])
+  
+  const sendMessageToModel = async (words) => {
+    setMessages((messages) => [...messages, { text: words }]);
     try {
       const response = await fetch(AZURE_BACKEND_URL, {
         method: "POST",
@@ -67,7 +60,7 @@ function App() {
               content: [
                 {
                   type: "text",
-                  text: promptValue,
+                  text: words,
                 },
               ],
             },
@@ -88,9 +81,14 @@ function App() {
     }
   };
 
+  SpeechRecognition.startListening({
+    continuous: true
+  });
+
   // adding chat component in full screen container
   return (
     <div className={styles.App}>
+      <audio ref={audioRef} src="/dinner-bell.wav"></audio>
       <section className={styles.topNav}>In Store Assistant</section>
       <section
         className={c(
@@ -118,7 +116,7 @@ function App() {
           inputMode="text"
           value={promptValue}
         />
-        <button onClick={startMicrophoneInput}>Record</button>
+        <button onClick={() => {}}>Record</button>
         <button onClick={sendMessageToModel}>Send</button>
       </section>
     </div>
